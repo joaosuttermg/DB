26b17c7a1374985fac6d21c51a36171b14d31b64
Arthur Giangiarulo <111184548+arthurGiangiarulo@users.noreply.github.com>
2024-10-09 20:30:00 -0300

    Implementação de rotas

--- App.js
+++ App.js
@@ -1,35 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HelloWorld from './src/screens/HelloWorld';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    // <div>
-    <View style={styles.container}>
-      {/* <p>, <h1>, <span> */}
-      <Text style={styles.hello}>Hello World</Text>
-      {/* Emoji sorrindo */}
-      <Text style={styles.emoji}>😀</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HelloWorld}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    // <div style="display: flex;">
-    backgroundColor: '#fff',
-    // <div style="background-color: #fff;">
-    alignItems: 'center',
-    // <div style="align-items: center;">
-    justifyContent: 'center',
-  },
-  hello: {
-    color: 'brown',
-    fontWeight: 'bold',
-    fontSize: 40,
-  },
-  emoji: {
-    fontSize: 50,
-  },
-});
---
+++ src/screens/HelloWorld.js
@@ -0,0 +1,29 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.hello}>Olá turma linda</Text>
+      <Text style={styles.emoji}>😀</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  hello: {
+    color: 'brown',
+    fontWeight: 'bold',
+    fontSize: 40,
+  },
+  emoji: {
+    fontSize: 50,
+  },
+});
