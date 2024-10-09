88d8074fefae6c309188c088642d8d5f608d0b55
Arthur Giangiarulo <111184548+arthurGiangiarulo@users.noreply.github.com>
2024-10-09 19:44:54 -0300

    First commit

--- App.js
+++ App.js
@@ -3,8 +3,12 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
+    // <div>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* <p>, <h1>, <span> */}
+      <Text style={styles.hello}>Hello World</Text>
+      {/* Emoji sorrindo */}
+      <Text style={styles.emoji}>😀</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +17,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    // <div style="display: flex;">
     backgroundColor: '#fff',
+    // <div style="background-color: #fff;">
     alignItems: 'center',
+    // <div style="align-items: center;">
     justifyContent: 'center',
+  },
+  hello: {
+    color: 'brown',
+    fontWeight: 'bold',
+    fontSize: 40,
+  },
+  emoji: {
+    fontSize: 50,
   },
 });
